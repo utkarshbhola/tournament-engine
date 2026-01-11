38f1fbded3dc4ad5a02c44cd2b9e4050424a0636
Utkarsh bhola <utkarsh.bhola@gmail.com>
2026-01-11 22:41:27 +0530

    fixed if else error

--- internal/engine/upgrade.go
+++ internal/engine/upgrade.go
@@ -5,12 +5,11 @@ import (
 	"tournament-engine/internal/model"
 )
 
-func applyUpgrade(f *model.Fighter, rng *rand.Rand){
-	if rngIntn(2) == 0{
+func applyUpgrade(f *model.Fighter, rng *rand.Rand) {
+	if rng.Intn(2) == 0 {
 		f.MaxHealth += 10
 		f.Health = f.MaxHealth
-	}
-	else{
+	} else {
 		f.Attack += 2
 	}
 }
